c3b8cb75e9ff38709dbc616b1c1ff7899f398a31
gebnfo <ge.georgesantos@hotmail.com>
2020-09-14 15:08:18 -0300

    correção dos campos do form do produto, request login para views produto

--- products/models.py
+++ products/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Product(models.Model):
-    nome = models.CharField(max_length=100)
-    categoria = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=9, decimal_places=2)
-    condicoesUso = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100, verbose_name="Nome")
+    categoria = models.CharField(max_length=100, verbose_name="Categoria")
+    descricao = models.CharField(max_length=100, verbose_name="Descrição")
+    preco = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Preço")
+    condicoesUso = models.CharField(max_length=100, verbose_name="Condições de uso")
 
     def __str__(self):
         return self.descricao
--- products/views.py
+++ products/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 from .models import Product
 from .forms import ProductForm
 
 
+@login_required
 def list_products(request):
     products = Product.objects.all()
     return render(request, 'products/products.html', {'products': products})
 
-
+@login_required
 def create_product(request):
     form = ProductForm(request.POST or None)
 
@@ -17,7 +20,7 @@ def create_product(request):
 
     return render(request, 'products/products-form.html', {'form': form})
 
-
+@login_required
 def update_product(request, id):
     product = Product.objects.get(id=id)
     form = ProductForm(request.POST or None, instance=product)
@@ -28,7 +31,7 @@ def update_product(request, id):
 
     return render(request, 'products/products-form.html', {'form': form, 'product': product})
 
-
+@login_required
 def delete_product(request, id):
     product = Product.objects.get(id=id)
 
