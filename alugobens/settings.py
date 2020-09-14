c63f13c7bbe10da8c97d5f90705ea4911176164e
gebnfo <ge.georgesantos@hotmail.com>
2020-09-13 22:53:49 -0300

    configurações deploy

--- alugobens/settings.py
+++ alugobens/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alugobens.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
