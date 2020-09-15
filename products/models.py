2b90d670d605c2f45c0130b14e532e235e61f825
gebnfo <ge.georgesantos@hotmail.com>
2020-09-15 12:26:52 -0300

    inserido a categoria

--- products/models.py
+++ products/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from users.models import MyUser
 
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100, verbose_name="Descricao")
+
+    def __str__(self):
+        return self.descricao
+
 class Product(models.Model):
     nome = models.CharField(max_length=100, verbose_name="Nome")
-    categoria = models.CharField(max_length=100, verbose_name="Categoria")
+    categoria = models.ForeignKey(Categoria, null=True, blank=True, on_delete=models.CASCADE)
     descricao = models.CharField(max_length=100, verbose_name="Descrição")
     preco = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Preço")
     condicoesUso = models.TextField(verbose_name="Condições de uso")
@@ -12,6 +19,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.descricao
+
+
 
 
 
