75d67feb759c32bd875194930c3d1e2f7238f1f4
gebnfo <ge.georgesantos@hotmail.com>
2020-09-29 20:30:15 -0300

    alterações

--- address/models.py
+++ address/models.py
@@ -4,17 +4,17 @@ from cpf_field.models import CPFField
 
 class Dados_usuario(models.Model):
 
-    cpf = cpf = CPFField('cpf', default='xxx.xxx.xxx-xx')
-    nome = models.CharField(max_length=255)
-    sobrenome = models.CharField(max_length=255)
-    data_nascimento = models.DateField(verbose_name='Data de Nascimento')
-    telefone = models.CharField(max_length=12)
-    cep = models.CharField(max_length=8, unique=True)    
-    cidade = models.CharField(max_length=255)
-    estado = models.CharField(max_length=2)
-    rua = models.CharField(max_length=255)
-    bairro = models.CharField(max_length=255)
-    complemento = models.CharField(max_length=255)
+    cpf = cpf = CPFField('cpf', null=True, blank=True)
+    nome = models.CharField(null=True, blank=True, max_length=255)
+    sobrenome = models.CharField(null=True, blank=True, max_length=255)
+    data_nascimento = models.DateField(null=True, blank=True, verbose_name='Data de Nascimento')
+    telefone = models.CharField(null=True, blank=True, max_length=12)
+    cep = models.CharField(null=True, blank=True, max_length=8, unique=True)
+    cidade = models.CharField(null=True, blank=True, max_length=255)
+    estado = models.CharField(null=True, blank=True, max_length=2)
+    rua = models.CharField(null=True, blank=True, max_length=255)
+    bairro = models.CharField(null=True, blank=True, max_length=255)
+    complemento = models.CharField(null=True, blank=True ,max_length=255)
     user = models.OneToOneField(MyUser, null=True, blank=True, on_delete=models.CASCADE)
     foto = models.ImageField(null=True, blank=True, verbose_name="Imagem")
 
