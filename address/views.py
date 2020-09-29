2373950f730404c891b65f4b03876fe7c2d790b0
gebnfo <ge.georgesantos@hotmail.com>
2020-09-29 20:40:57 -0300

    correção no perfil

--- address/views.py
+++ address/views.py
@@ -13,14 +13,10 @@ def alterar_perfil(request,userId):
     except ObjectDoesNotExist:
         perfil = Dados_usuario()
     form = Dados_usuarioForm(request.POST or None, request.FILES or None, instance=perfil)
-    if request.POST:
-        if form.is_valid():
-            dados_usuario = form.save(commit=False)
-            dados_usuario.user = user
-            dados_usuario.save()
-            return redirect(reverse('perfil', kwargs={'userId': user.id}))
-        else:
-            messages.error(request, 'CPF inválido, por favor insira um CPF valido!')
-            return render(request, 'user/perfil.html', {'form': form, 'perfil': perfil})
+    if form.is_valid():
+        dados_usuario = form.save(commit=False)
+        dados_usuario.user = user
+        dados_usuario.save()
+        return redirect(reverse('perfil', kwargs={'userId': user.id}))
 
     return render(request, 'user/perfil.html', {'form': form, 'perfil': perfil})
