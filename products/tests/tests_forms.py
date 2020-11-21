cfff83b3cd163685b737204b851b41e0790b1d16
josuejorge2099hotmailcom <josuejorge2099@hotmail.com>
2020-11-20 23:40:48 -0300

    teste unitário para form alugar e denuncia

--- products/tests/tests_forms.py
+++ products/tests/tests_forms.py
@@ -51,4 +51,80 @@ class ProductFormTestCase(TestCase):
             })        
         self.assertFalse(form.is_valid())
         
+    def test_form_product_category_invalid(self):        
+        form = ProductForm(data={
+        'nome': 'serra eletrica',
+        'categoria': 'jogos',
+        'descricao': 'nova com manual de instrucao',
+        'preco': '45',
+        'condicoesUso': 'nova',               
+        'estado':'Pernambuco',
+        'cidade':'Recife'         
+        })      
 
+        self.assertFalse(form.is_valid())
+    
+    def test_form_denuncia_valid(self):
+        form = DenunciaForm(data={
+            'motivo': 'produto ilegal',
+            'descricao': 'ilícitos'
+        })        
+        self.assertTrue(form.is_valid())
+
+    def test_form_denuncia_invalid(self):
+        form = DenunciaForm(data={})            
+        self.assertFalse(form.is_valid())
+
+    def test_form_denuncia_partially_invalid_case_one(self):
+        form = DenunciaForm(data={
+            'motivo': 'ilicito'
+        })            
+        self.assertFalse(form.is_valid())
+    
+    def test_form_denuncia_partially_invalid_case_two(self):
+        form = DenunciaForm(data={
+            'descricao': 'ilícitos'
+        })            
+        self.assertFalse(form.is_valid())
+
+    def test_form_alugar_invalid(self):
+        form = AlugarForm(data={})            
+        self.assertFalse(form.is_valid())
+
+    def test_form_alugar_valid(self):
+        inicio = datetime(2020, 12, 12)
+        fim =  datetime(2020, 12, 15)
+
+        form = AlugarForm(data={
+            'inicio': inicio,
+            'fim': fim
+        })            
+        self.assertTrue(form.is_valid())
+
+    def test_form_alugar_date_incomplete(self):        
+        inicio = datetime(2020, 12, 12)
+        fim =  datetime(2020, 12, 15)
+
+        form = AlugarForm(data={
+            'inicio': inicio            
+        })            
+        self.assertFalse(form.is_valid())
+
+    def test_form_alugar_date_now_less_date_begin(self): 
+        data = datetime.now() 
+        inicio = datetime(2020, 10, 10)
+        fim =  datetime(2020, 12, 15)
+        
+        certo = False
+        if inicio > data: 
+            certo = True           
+            form = AlugarForm(data={
+            'inicio': inicio,
+            'fim': fim           
+        })   
+        #esse teste passou, mas está errado,
+        #não pode permitir datas passadas
+        #esse assert precisa ser True, para validar que não é permitido datas passadas         
+        self.assertFalse(certo)
+        
+       
