47c2b3b532f2d587f8712b060b09901d7f2f1446
gebnfo <ge.georgesantos@hotmail.com>
2020-11-26 21:16:31 -0300

    configuração postgreSql

--- alugobens/settings.py
+++ alugobens/settings.py
@@ -89,6 +89,18 @@ default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'alugo',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#   }
+#}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
